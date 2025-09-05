d7ccbe149866bbce722bede2854da9098c9c1fd5
TAMNI <tamni@oocl.com>
2025-09-05 16:02:51 +0800

    feat: build function for move when direction is N

--- src/main/java/com/afs/tdd/Location.java
+++ src/main/java/com/afs/tdd/Location.java
@@ -1,19 +1,39 @@
 package com.afs.tdd;
 
 public class Location {
+
+    private int x;
+    private int y;
+    private Direction direction;
+
     public Location(int x, int y, Direction direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
     }
 
     public int getLocationY() {
-        return 0;
+        return this.y;
     }
 
     public int getLocationX() {
-        return 0;
+        return this.x;
     }
 
     public Direction getDirection() {
-        return null;
+        return this.direction;
+    }
+
+    public void setX(int newX){
+        this.x = newX;
+    }
+
+    public void setY(int newY){
+        this.y = newY;
+    }
+
+    public void setDirection(Direction newDirection){
+        this.direction = newDirection;
     }
 }
 
--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -7,12 +7,50 @@ public class MarsRover {
     private Location location;
 
     public MarsRover(Location location) {
+        this.location = location;
     }
 
     public Location getLocation() {
-        return null;
+        return this.location;
     }
 
-    public void executeCommand(Command m) {
+    public void executeCommand(Command command) {
+        switch (command) {
+            case M: move();
+            case L: turnLeft();
+            case R: turnRight();
+        }
     }
+
+    public void move() {
+        switch (this.location.getDirection()) {
+            case N: this.location.setY(this.location.getLocationY()+1);
+            case E: ;
+            case S: ;
+            case W: ;
+        }
+    }
+
+    public void turnLeft() {
+        switch (this.location.getDirection()) {
+            case N: ;
+            case E: ;
+            case S: ;
+            case W: ;
+        }
+    }
+
+    public void turnRight() {
+        switch (this.location.getDirection()) {
+            case N: ;
+            case E: ;
+            case S: ;
+            case W: ;
+        }
+    }
+
+
+
+
+
 }
