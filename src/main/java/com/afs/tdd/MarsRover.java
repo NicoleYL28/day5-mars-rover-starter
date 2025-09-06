d158f3f9db8519394aa0b149db489a9e85a99dd7
TAMNI <tamni@oocl.com>
2025-09-06 17:35:09 +0800

    refactor: build function for turn left when direction is N

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,6 +1,8 @@
 package com.afs.tdd;
 
 import javax.swing.plaf.basic.BasicArrowButton;
+import java.util.Arrays;
+import java.util.List;
 
 public class MarsRover {
     
@@ -16,9 +18,9 @@ public class MarsRover {
 
     public void executeCommand(Command command) {
         switch (command) {
-            case M: move();
-            case L: turnLeft();
-            case R: turnRight();
+            case M: move(); break;
+            case L: turnLeft(); break;
+            case R: turnRight(); break;
         }
     }
 
@@ -28,12 +30,13 @@ public class MarsRover {
             case E: this.location.setX(this.location.getLocationX()+1); break;
             case S: this.location.setY(this.location.getLocationY()-1); break;
             case W: this.location.setX(this.location.getLocationX()-1); break;
-
         }
     }
 
     public void turnLeft() {
-
+        switch (this.location.getDirection()) {
+            case N: this.location.setDirection(Direction.W);
+        };
     }
 
     public void turnRight() {
