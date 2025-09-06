a6ac9ca214d55e968132546e95c1749cac7edc39
TAMNI <tamni@oocl.com>
2025-09-06 17:38:54 +0800

    test: build should_turn_left_when_executeCommand_given_L_when_direction_is_S test case

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -106,4 +106,21 @@ class DemoTest {
         assertEquals(currentLocation.getDirection(), marsRover.getLocation().getDirection());
 
     }
+
+    @Test
+    void should_turn_left_when_executeCommand_given_L_when_direction_is_S() {
+        //Given
+        Location location = new Location(0, 0, Direction.S);
+        MarsRover marsRover = new MarsRover(location);
+
+        //when
+        marsRover.executeCommand(Command.L);
+
+        //Then
+        Location currentLocation = new Location(0, 0, Direction.E);
+        assertEquals(currentLocation.getLocationY(), marsRover.getLocation().getLocationY());
+        assertEquals(currentLocation.getLocationX(), marsRover.getLocation().getLocationX());
+        assertEquals(currentLocation.getDirection(), marsRover.getLocation().getDirection());
+
+    }
 }
